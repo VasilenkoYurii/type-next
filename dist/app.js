ff305349fa4c68481e5295696b41cdd6af99158a
VasilenkoYurii <yura.vasilenko98@gmail.com>
2023-06-19 16:49:27 +0300

    added new fun

--- dist/app.js
+++ dist/app.js
@@ -59,4 +59,35 @@ const page2 = {
     accounts: ['Alex'],
     status: 'close',
 };
+function getPromise() {
+    return new Promise(resolve => {
+        resolve(['Text', 50]);
+    });
+}
+getPromise().then(data => {
+    console.log(data);
+});
+function compare(top, bottom) {
+    return {
+        name: top.name,
+        color: top.color,
+        position: bottom.position,
+        weight: bottom.weight,
+    };
+}
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+const merged = merge({ name: 'Alisa' }, { age: 28 });
+console.log(merged.name);
+class Component {
+    constructor(props) {
+        this.props = props;
+    }
+}
+class PageComponent extends Component {
+    pageInfo() {
+        console.log(this.props.title);
+    }
+}
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -84,3 +84,62 @@ const page2: Page = {
   accounts: ['Alex'],
   status: 'close',
 };
+
+function getPromise(): Promise<Array<string | number>> {
+  return new Promise(resolve => {
+    resolve(['Text', 50]);
+  });
+}
+
+getPromise().then(data => {
+  console.log(data);
+});
+
+//------------------------------------------------
+
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number;
+};
+
+// Є функція, вона приймає два аргументи, в один потрапляє name і color, в іншу частину - position і weight. Потрібно явно вказати, що ці поля з AllType. І сама функція повертає AllType. Скористайтеся Pick.
+
+function compare(
+  top: Pick<AllType, 'name' | 'color'>,
+  bottom: Pick<AllType, 'position' | 'weight'>
+): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  };
+}
+
+// ---------------------------------------
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const merged = merge({ name: 'Alisa' }, { age: 28 });
+
+console.log(merged.name);
+
+//---------------------------------------------
+
+class Component<T> {
+  constructor(public props: T) {}
+}
+
+interface IProps {
+  title: string;
+}
+
+class PageComponent extends Component<IProps> {
+  pageInfo() {
+    console.log(this.props.title);
+  }
+}
